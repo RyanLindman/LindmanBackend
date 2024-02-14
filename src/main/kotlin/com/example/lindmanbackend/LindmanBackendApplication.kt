caad063cdc3597384b1bab865b12c9346189075e
RyanLindman <ryan.lindman@outlook.com>
2024-02-14 23:30:12 +0100

    started a project

---
+++ src/main/kotlin/com/example/lindmanbackend/LindmanBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lindmanbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LindmanBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<LindmanBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/lindmanbackend/LindmanBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lindmanbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LindmanBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
